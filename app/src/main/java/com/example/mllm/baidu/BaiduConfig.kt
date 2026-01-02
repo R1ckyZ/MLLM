e30f5130ed327119a050fbfd1914401436a9d51a
Ricky <ricky@RickydeMacBook-Pro.local>
2026-01-03 05:02:00 +0800

    Revert "upload project files"
    This reverts commit 59da492c5165f7b70f53afb73f472107c70bd7d7.

--- app/src/main/java/com/example/mllm/baidu/BaiduConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mllm.baidu
-
-data class BaiduConfig(
-    val appId: String,
-    val appKey: String,
-    val secretKey: String,
-)
--- app/src/main/java/com/example/mllm/device/Screenshot.kt
+++
@@ -1,8 +0,0 @@
-package com.example.mllm.device
-
-data class Screenshot(
-    val base64Data: String,
-    val width: Int,
-    val height: Int,
-    val isSensitive: Boolean = false,
-)
